2aaee7d6ae404d85982b4e39fc33045bc1398965
Joey Saha <joeysaha@hotmail.com>
2018-11-28 22:38:53 -0500

    combining skills

--- pacman-defining-and-calling-functions.js
+++ pacman-defining-and-calling-functions.js
@@ -29,3 +29,7 @@ function addHighScore(highScoreList, playerInitials, score) {
   var record = { player: playerInitials, score: score };
   highScoreList.push(record); // add record
 }
+addHighScore(highScores, 'SDL', 2000);
+console.log(highScores);
+addHighScore(highScores, 'JSL', 10000);
+console.log(highScores);
